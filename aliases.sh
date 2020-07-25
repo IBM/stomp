c34d803cb74f0336a5c8e87a3139ec6ec0f1c963
Aporva Amarnath <aporvaa@umich.edu>
2020-07-25 19:34:09 -0400

    kill command

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,5 @@
+
 alias run="./utils/run_all_2.py"
 alias collect="./utils/collect.py"
 alias viz="./utils/gantt_plot.py"
+alias killpython="ps -u $(whoami) | grep python | cut -c-6 | xargs kill -9"
